da03376ecd68ca84a54a8f4dd47466262fdb1cd6
GabiHawk90 <80804518+GabiHawk90@users.noreply.github.com>
2021-12-06 17:33:28 -0700

    starting to build out code for argon, need to fix somethings

--- CapStone_Alarm/src/CapStone_Alarm.ino
+++ CapStone_Alarm/src/CapStone_Alarm.ino
@@ -5,16 +5,62 @@
  * Date:
  */
 
+bool BmeStatus;
 
+const int blueBut = 13;
 const int teen = D16;
 
+void setup()
+{
+  pinMode(teen, OUTPUT);
+}
+void loop()
+{
+  digitalWrite(teen, HIGH);
+  delay(500);
+  digitalWrite(teen, LOW);
+  delay(500);
+}
 
-void setup(){
-  pinMode(teen,OUTPUT);
+bool buttonState = digitalRead(blueBut);
+if (buttonState){
+  showEmergCall();
+  digitalWrite(teen, HIGH);
 }
-void loop (){
-   digitalWrite (teen,HIGH);
-   delay(500);
-   digitalWrite (teen,LOW);
-   delay(500);
+else{
+  Serial.printf("Darkness\n");
+  digitalWrite(teen, LOW);
 }
+}
+void showEmergCall(void) {
+  display.clearDisplay();
+  display.setTextSize(3);
+  display.setTextColor(SSD1306_WHITE);
+  display.setRotation(0);
+  display.setCursor(10, 7);
+  display.printf("Dialed", 47, 47);
+  display.display();
+  delay(800);
+
+  display.clearDisplay();
+  display.setTextSize(3);
+  display.setTextColor(SSD1306_WHITE);
+  display.setRotation(0);
+  display.setCursor(35, 10);
+  display.printf("911", 47, 47);
+  display.display();
+  delay(800);
+}
+
+
+void startObjects() {
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;); //Dont proceed, loop forever
+  }
+
+  BmeStatus = bme.begin (BMEADDRESS);
+  if (BmeStatus == false) {
+    Serial.printf("BME280 at address 0x%02 X failed tostart", BMEADDRESS);
+  }
+
