90b63aa29fd0815ea45bc8cfd6766f925321763a
GabiHawk90 <80804518+GabiHawk90@users.noreply.github.com>
2021-12-09 16:04:05 -0700

    fixing bugs

--- CapStone_Alarm/src/CapStone_Alarm.ino
+++ CapStone_Alarm/src/CapStone_Alarm.ino
@@ -1,58 +1,52 @@
 /*
  * Project CapStone_Alarm
- * Description:
- * Author:
- * Date:
+ * Description:a portable alarm call system
+ * Author:Gabriella Hawkins
+ * Date:12/9/21
  */
-
+#include "credentials.h"
+#include "Adafruit_MQTT/Adafruit_MQTT.h"
+#include "Adafruit_MQTT/Adafruit_MQTT_SPARK.h"
 
 int buttonState;
-int buttonStateAlarm;
 
-const int hallPin = A5;
-const int ledRed = D5;
-const int ledBlue = D6;
 const int blueBut = A3;
-const int greenBut = A4;
 const int teen = D4;
 
 
 
+ SYSTEM_MODE(SEMI_AUTOMATIC); // Uncomment if using without Wifi
 
-void setup()
-{
+ TCPClient TheClient;
 
-  pinMode(hallPin, INPUT);
-  pinMode(ledRed, OUTPUT);
-  pinMode(ledBlue, OUTPUT);
-  pinMode(teen, OUTPUT);
-  pinMode(blueBut, INPUT);
-  pinMode(greenBut, INPUT);
-}
-void loop()
-{
+ Adafruit_MQTT_SPARK mqtt(&TheClient,AIO_SERVER,AIO_SERVERPORT,AIO_USERNAME,AIO_KEY);
+ Adafruit_MQTT_Publish mqttCall911=Adafruit_MQTT_Publish(&mqtt,AIO_USERNAME"/feeds/911");
 
-  if (buttonStateAlarm == HIGH)
-  {
-    if (digitalRead(hallPin))
-    {
-      digitalWrite(ledRed, HIGH);
-    }
-    else
-    {
-      digitalWrite(ledBlue, LOW);
-    }
-  }
 
-  buttonState = digitalRead(blueBut);
-  if (buttonState)
-  {
+void setup(){
+ Serial.begin(9600);
+ waitFor( Serial.isConnected,15000); // wait for Serial Monitor to startup
+
+ WiFi. connect () ; // Connect to internet , but not Particle Cloud
+ while(WiFi.connecting()){
+ Serial.printf(".");
+ }
+  pinMode(teen,OUTPUT);
+  pinMode(blueBut,INPUT);
+ }
+void loop(){
+ buttonState = digitalRead(blueBut);
+  if (buttonState){ 
+    mqttCall911.publish(911);// Publishing to a MQTT feed
+ if(mqtt.Update()){ // if mqtt object ( Adafruit .io) is available to receive data
+ Serial.printf("Publishing %i to Adafruit.io feed FeedNameB\n",mqttCall911);
+ mqttCall911.publish(911);
+ }
     digitalWrite(teen, HIGH);
     Serial.printf("OH HEY!\n");
   }
   else{
     digitalWrite(teen, LOW);
-
   }
 }
 
