e910b21bf78fa8a58d1be5b69bcc017088e24e49
GabiHawk90 <80804518+GabiHawk90@users.noreply.github.com>
2021-12-08 17:47:41 -0700

    almost ready

--- CapStone_Alarm/src/CapStone_Alarm.cpp
+++ CapStone_Alarm/src/CapStone_Alarm.cpp
@@ -13,17 +13,55 @@
 
 
 void setup();
-void loop ();
+void loop();
 #line 9 "c:/Users/gabi/Documents/IoT/CapStone/CapStone_Alarm/src/CapStone_Alarm.ino"
-const int teen = D16;
+int buttonState;
+int buttonStateAlarm;
+
+const int hallPin = A5;
+const int ledRed = D5;
+const int ledBlue = D6;
+const int blueBut = A3;
+const int greenBut = A4;
+const int teen = D4;
 
 
-void setup(){
-  pinMode(teen,OUTPUT);
+
+
+void setup()
+{
+
+  pinMode(hallPin, INPUT);
+  pinMode(ledRed, OUTPUT);
+  pinMode(ledBlue, OUTPUT);
+  pinMode(teen, OUTPUT);
+  pinMode(blueBut, INPUT);
+  pinMode(greenBut, INPUT);
 }
-void loop (){
-   digitalWrite (teen,HIGH);
-   delay(500);
-   digitalWrite (teen,LOW);
-   delay(500);
+void loop()
+{
+
+  if (buttonStateAlarm == HIGH)
+  {
+    if (digitalRead(hallPin))
+    {
+      digitalWrite(ledRed, HIGH);
+    }
+    else
+    {
+      digitalWrite(ledBlue, LOW);
+    }
+  }
+
+  buttonState = digitalRead(blueBut);
+  if (buttonState)
+  {
+    digitalWrite(teen, HIGH);
+    Serial.printf("OH HEY!\n");
+  }
+  else{
+    digitalWrite(teen, LOW);
+
+  }
 }
+
--- CapStone_Alarm/src/CapStone_Alarm.ino
+++ CapStone_Alarm/src/CapStone_Alarm.ino
@@ -5,62 +5,54 @@
  * Date:
  */
 
-bool BmeStatus;
 
-const int blueBut = 13;
-const int teen = D16;
+int buttonState;
+int buttonStateAlarm;
+
+const int hallPin = A5;
+const int ledRed = D5;
+const int ledBlue = D6;
+const int blueBut = A3;
+const int greenBut = A4;
+const int teen = D4;
+
+
+
 
 void setup()
 {
+
+  pinMode(hallPin, INPUT);
+  pinMode(ledRed, OUTPUT);
+  pinMode(ledBlue, OUTPUT);
   pinMode(teen, OUTPUT);
+  pinMode(blueBut, INPUT);
+  pinMode(greenBut, INPUT);
 }
 void loop()
 {
-  digitalWrite(teen, HIGH);
-  delay(500);
-  digitalWrite(teen, LOW);
-  delay(500);
-}
 
-bool buttonState = digitalRead(blueBut);
-if (buttonState){
-  showEmergCall();
-  digitalWrite(teen, HIGH);
-}
-else{
-  Serial.printf("Darkness\n");
-  digitalWrite(teen, LOW);
-}
-}
-void showEmergCall(void) {
-  display.clearDisplay();
-  display.setTextSize(3);
-  display.setTextColor(SSD1306_WHITE);
-  display.setRotation(0);
-  display.setCursor(10, 7);
-  display.printf("Dialed", 47, 47);
-  display.display();
-  delay(800);
-
-  display.clearDisplay();
-  display.setTextSize(3);
-  display.setTextColor(SSD1306_WHITE);
-  display.setRotation(0);
-  display.setCursor(35, 10);
-  display.printf("911", 47, 47);
-  display.display();
-  delay(800);
-}
-
-
-void startObjects() {
-  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for (;;); //Dont proceed, loop forever
+  if (buttonStateAlarm == HIGH)
+  {
+    if (digitalRead(hallPin))
+    {
+      digitalWrite(ledRed, HIGH);
+    }
+    else
+    {
+      digitalWrite(ledBlue, LOW);
+    }
   }
 
-  BmeStatus = bme.begin (BMEADDRESS);
-  if (BmeStatus == false) {
-    Serial.printf("BME280 at address 0x%02 X failed tostart", BMEADDRESS);
+  buttonState = digitalRead(blueBut);
+  if (buttonState)
+  {
+    digitalWrite(teen, HIGH);
+    Serial.printf("OH HEY!\n");
   }
+  else{
+    digitalWrite(teen, LOW);
+
+  }
+}
 
